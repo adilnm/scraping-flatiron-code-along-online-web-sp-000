25f40aee6bd4aded02c74202363c5ff4bcd1e97c
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-27 07:35:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,11 @@ class Scraper
     def get_page
       html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
       doc = Nokogiri::HTML(html)
-      binding.pry
+      doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
     end
     
     def get_courses
