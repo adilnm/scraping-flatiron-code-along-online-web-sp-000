cf987287a16e48482d5b7c0409a3f0dfe8c374cd
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-27 07:24:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,21 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
   
+    def get_page
+      html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+      doc = Nokogiri::HTML(html)
+      binding.pry
+    end
+    
+    def get_courses
+      
+    end
+    
     def print_courses
     self.make_courses
     Course.all.each do |course|
